de52ab6994358a61fb236ddc8f742927db93e987
Peter <75426240+Gingerfridge@users.noreply.github.com>
2021-11-10 09:39:30 +0000

    Option instead of the rotation option

---
+++ simulation_frame_producer.m
@@ -0,0 +1,5 @@
+function simulation_frame_producer
+% this function will create the files needed to be sliced etc
+% [d,Output1] = system("wsl gmx editconf -f " + protein_filename + " -o " + protein_nickname + "_" + x_rot + "_" + y_rot + "_" + z_rot + ".gro -rotate " + x_rot + " " + y_rot + " " + z_rot );
+% all that is needed is a list of file names that correspond to gro files
+
