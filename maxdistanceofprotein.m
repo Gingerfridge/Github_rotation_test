9b1031948fb512c4afdcae0b34abb8573c11d6df
Peter <75426240+Gingerfridge@users.noreply.github.com>
2021-11-11 09:59:33 +0000

    Fixed issue with using the contrast index rather than the number of frames

--- maxdistanceofprotein.m
+++ maxdistanceofprotein.m
@@ -1,5 +1,6 @@
 function [output] = maxdistanceofprotein(Thick_protein_slice)
-
-for i = 1:length(Thick_protein_slice)
+%fixed this function to work on the number of frames not contrasts
+number_of_frames = size(Thick_protein_slice);
+for i = 1:number_of_frames(1,1)
     output(i,1) = (Thick_protein_slice{i,1}(end,1));
 end
